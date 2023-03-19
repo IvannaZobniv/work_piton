e3fe9d22ae2e1e7ab7d5e7e51bf0cc37ff4ca488
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-03-19 08:30:43 +0200

    fale lab is finish

--- Create_kml.py
+++ Create_kml.py
@@ -1,5 +1,6 @@
 import math
-f = open('data.txt')
+import numpy as np
+f = open('Data1.txt')
 f1 = open('data_out.kml', 'w')
 Mag = []
 List_count = 0
@@ -18,6 +19,10 @@ while True:
         break
     List_count = List_count + 1
     Mag.append(float(Zn))
+
+Tmax = max(Mag)
+Tmin = min(Mag)
+Ts = Tmin+ (Tmax-Tmin)/2
 
 deltaX = float(BX)-float(AX)
 deltaY = float(BY)-float(AY)
@@ -42,8 +47,8 @@ f1.write('        <extrude>1</extrude>' + '\n')
 s ='       <coordinates>'
 i = 0
 while i <List_count:
-    s = s + str(i*delta*math.cos(kut) - (Mag[i]-50700)*0.00002*math.sin(kut)+float(AX))+ ','
-    s = s + str(i * delta * math.sin(kut) + (Mag[i] - 50700) * 0.00002 * math.cos(kut) + float(AY)) + ',0 '
+    s = s + str(i*delta*math.cos(kut) - (Mag[i]-Ts)*0.00002*math.sin(kut)+float(AX)) + ','
+    s = s + str(i * delta * math.sin(kut) + (Mag[i] - Ts) * 0.00002 * math.cos(kut) + float(AY)) + ',0 '
     i = i+1
 s=s + '</coordinates>' + '\n'
 f1.write(s)
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
+with open('Data1.txt') as f, open('data_out.kml', 'w') as f1:
+    AY, AX, BY, BX = [float(f.readline()) for _ in range(4)]
+    Mag = [float(line) for line in f if is_float(line)]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    Ts = (max(Mag) + min(Mag)) / 2
+    delta = math.sqrt((BX - AX)**2 + (BY - AY)**2) / len(Mag)
+    kut = math.atan(BY - AY) / (BX - AX)
 
+    coordinates = ''.join(f"{i * delta * math.cos(kut) - (mag - Ts) * 0.00002 * math.sin(kut) + AX},"
+                          f"{i * delta * math.sin(kut) + (mag - Ts) * 0.00002 * math.cos(kut) + AY},0 "
+                          for i, mag in enumerate(Mag))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    kml_content = f"""<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.2">
+  <Document>
+    <Placemark>
+      <name>Міст Патона</name>
+      <description>Графік магнітного поля</description>
+      <Style>
+        <LineStyle>
+          <color>A600FF00</color>
+          <width>3</width>
+        </LineStyle>
+      </Style>
+      <LineString>
+        <extrude>1</extrude>
+        <coordinates>{coordinates}</coordinates>
+      </LineString>
+    </Placemark>
+  </Document>
+</kml>
+"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    f1.write(kml_content)
+
